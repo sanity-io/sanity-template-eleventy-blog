bbaa8697d9f0ba1eb12d61580061780391ca6870
Nicolas Goutay <phacks@users.noreply.github.com>
2020-07-13 10:30:52 +0200

    Fix link to “ Introduction to Portable Text” guide

--- template/web/utils/serializers.js
+++ template/web/utils/serializers.js
@@ -1,6 +1,6 @@
 const imageUrl = require('./imageUrl')
 
-// Learn more on https://www.sanity.io/docs/guides/introduction-to-portable-text
+// Learn more on https://www.sanity.io/guides/introduction-to-portable-text
 module.exports = {
   types: {
     authorReference: ({node}) => `[${node.name}](/authors/${node.slug.current})`,
